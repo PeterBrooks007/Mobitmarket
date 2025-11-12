8d351807a69fc9d7cd0c2bebfaabd9940223fb5b
Peter Brooks <digitalyinvestassets@gmail.com>
2025-11-12 08:26:22 +0100

    my first commit

---
+++ backend/utils/sendEmail.js
@@ -0,0 +1,53 @@
+const nodemailer = require("nodemailer");
+const MailGen = require("mailgen");
+
+const sendEmail = async (subject, send_to, template, reply_to, cc) => {
+  // Create Email Transporter
+  const transporter = nodemailer.createTransport({
+    // service: "gmail",
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    secure: true,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  //Create Template with Mailgen
+  const mailGenerator = new MailGen({
+    theme: "default",
+    product: {
+      name: "mobitmarket Platform",
+      link: "https://mobitmarket.com/",
+      // Optional product logo
+      logo: "https://res.cloudinary.com/dkeqxypfr/image/upload/v1762931937/logo_l6ncvp.png",
+      // Custom logo height
+      logoHeight: "70px",
+    },
+  });
+
+  const emailTemplate = mailGenerator.generate(template);
+  require("fs").writeFileSync("preview.html", emailTemplate, "utf8");
+
+  //Options for sending email
+  const options = {
+    from: `mobitmarket <${process.env.EMAIL_USER}>`,
+    to: send_to,
+    replyTo: reply_to,
+    subject,
+    html: emailTemplate,
+    cc,
+  };
+
+  //send Email
+  transporter.sendMail(options, function (err, info) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(info);
+    }
+  });
+};
+
+module.exports = sendEmail;
